24d0ce6963700350a1703f8b7dbb1cd9ccc25abb
Matheus Cirillo <matheuscirillo@usp.br>
2023-10-13 16:32:47 -0300

    better message flow
    can now continuously communicate

--- Makefile
+++ Makefile
@@ -8,8 +8,7 @@ VDFLAGS += --leak-check=full --show-leak-kinds=all -s
 SRCDIRS = $(sort $(dir $(wildcard src/*/)))
 INC_PARAMS = $(addprefix -I ,$(SRCDIRS))
 
-PEER1 ?= build/peer1
-PEER2 ?= build/peer2
+program ?= build/program
 
 ZIPFILE    ?= ../zipfile.zip
 CFILES      = $(wildcard src/*.c) $(wildcard src/**/*.c)
@@ -17,7 +16,7 @@ CFILES      = $(wildcard src/*.c) $(wildcard src/**/*.c)
 .PHONY: all clean zip run test debug
 
 all: PDFLAGS += -O3
-all: $(PEER1) $(PEER2)
+all: $(program)
 
 clean:
 	@rm -f $(ZIPFILE)
@@ -26,23 +25,16 @@ clean:
 zip: clean
 	$(ZIP) a $(ZIPFILE) ./*
 
-run1: $(PEER1)
-	@./$(PEER1) $(ARGS)
-
-run2: $(PEER2)
-	@./$(PEER2) $(ARGS)
+run: $(program)
+	@./$(program) $(ARGS)
 
 debug: CFLAGS+= -g
 debug: clean
-debug: $(PEER1)
+debug: $(program)
 
-$(PEER1): $(CFILES)
+$(program): $(CFILES)
 	@mkdir -p build
 	$(CC) $(CFLAGS) $(INC_PARAMS) -o $@ $^ $(LDFLAGS) $(PDFLAGS)
 
-$(PEER2): $(CFILES)
-	@mkdir -p build
-	$(CC) $(CFLAGS) $(INC_PARAMS) -o $@ $^ $(LDFLAGS) $(PDFLAGS)
-
-valgrind: $(PEER1)
-	valgrind $(VDFLAGS) ./$(PEER1)
+valgrind: $(program)
+	valgrind $(VDFLAGS) ./$(program)
--- src/main.c
+++ src/main.c
@@ -10,17 +10,19 @@
 
 char name[20];
 int PORT;
+int PORT_server;
 
 void sending();
+void changePort();
 void receiving(int server_fd);
 void *receive_thread(void *server_fd);
 
 int main()
 {
-    printf("Enter name:");
+    printf("Enter name: ");
     scanf("%s", name);
 
-    printf("Enter your port number:");
+    printf("Enter your port number: ");
     scanf("%d", &PORT);
 
     int server_fd;
@@ -41,6 +43,8 @@ int main()
     //Printed the server socket addr and port
     printf("IP address is: %s\n", inet_ntoa(address.sin_addr));
     printf("port is: %d\n", (int)ntohs(address.sin_port));
+    printf("To change port number enter @#1$ at any moment\n");
+    printf("To exit enter @#2$ at any moment\n");
 
     if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0)
     {
@@ -52,72 +56,78 @@ int main()
         perror("listen");
         exit(EXIT_FAILURE);
     }
-    int ch;
     pthread_t tid;
     pthread_create(&tid, NULL, &receive_thread, &server_fd); //Creating thread to keep receiving message in real time
-    printf("\n*****At any point in time press the following:*****\n1.Send message\n0.Quit\n");
-    printf("\nEnter choice:");
-    do
-    {
 
-        scanf("%d", &ch);
-        switch (ch)
-        {
-        case 1:
-            sending();
-            break;
-        case 0:
-            printf("\nLeaving\n");
-            break;
-        default:
-            printf("\nWrong choice\n");
-        }
-    } while (ch);
+    changePort();
+
+    sending();
 
     close(server_fd);
 
     return 0;
 }
 
+void changePort()
+{
+    printf("Enter the port to send message: ");
+    scanf("%d", &PORT_server);
+}
+
 //Sending messages to port
 void sending()
 {
-
     char buffer[2000] = {0};
     //Fetching port number
-    int PORT_server;
 
-    //IN PEER WE TRUST
-    printf("Enter the port to send message:"); //Considering each peer will enter different port
-    scanf("%d", &PORT_server);
+    char message[1024] = {0};
+    char dummy;
+    //printf("Enter your message:");
+    scanf("%c", &dummy); //The buffer is our enemy
+    scanf("%[^\n]s", message);
+
+    //if message is @#1$ then change port
+    if (strcmp(message, "@#1$") == 0)
+    {
+        fputs("\033[A\033[2K",stdout);
+        rewind(stdout);
+        changePort();
+        sending();
+    } //if message is @#2$ then exit
+    else if (strcmp(message, "@#2$") == 0)
+    {
+        exit(0);
+    }
 
     int sock = 0;
     struct sockaddr_in serv_addr;
-    char hello[1024] = {0};
-    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    while ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
     {
         printf("\n Socket creation error \n");
-        return;
+        changePort();
     }
+
+    sprintf(buffer, "%s[PORT:%d] says: %s", name, PORT, message);
 
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = INADDR_ANY; //INADDR_ANY always gives an IP of 0.0.0.0
     serv_addr.sin_port = htons(PORT_server);
 
-    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+    while (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
     {
         printf("\nConnection Failed \n");
-        return;
+        changePort();
     }
 
-    char dummy;
-    printf("Enter your message:");
-    scanf("%c", &dummy); //The buffer is our enemy
-    scanf("%[^\n]s", hello);
-    sprintf(buffer, "%s[PORT:%d] says: %s", name, PORT, hello);
     send(sock, buffer, sizeof(buffer), 0);
-    printf("\nMessage sent\n");
+    //unicode checkmark in the end of the previous line
+    
+    fputs("\033[A\033[2K",stdout);
+    rewind(stdout);
+
+    printf("%s -> [PORT:%d] says: %s \u2713 \n", name, PORT_server, message);
     close(sock);
+    sending();
 }
 
 //Calling receiving every 2 seconds
